40a0f75222c11adc9a5325b8f4ae931517addeb9
hejiehao <hejiehao12@126.com>
2024-07-30 20:45:29 +0800

    Update

--- src/main.py
+++ src/main.py
@@ -110,30 +110,6 @@ async def show_join_date(interaction: discord.Interaction, member: discord.Membe
     # The format_dt function formats the date time into a human readable representation in the official client
     await interaction.response.send_message(f'{member} 在 {discord.utils.format_dt(member.joined_at)} 加入了这个服务器')
 
-
-# This context menu command only works on messages
-@client.tree.context_menu(name='举报消息')
-async def report_message(interaction: discord.Interaction, message: discord.Message):
-    # We're sending this response message with ephemeral=True, so only the command executor can see it
-    await interaction.response.send_message(
-        f'感谢您给 Moderator 举报了 {message.author.mention} 的消息', ephemeral=True
-    )
-
-    # Handle report by sending it into a log channel
-    log_channel = interaction.guild.get_channel(os.environ['LOG_CHANNEL_ID'])  # replace with your log channel id
-
-    embed = discord.Embed(title='举报的消息')
-    if message.content:
-        embed.description = message.content
-
-    embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
-    embed.timestamp = message.created_at
-
-    url_view = discord.ui.View()
-    url_view.add_item(discord.ui.Button(label='跳转到这条消息', style=discord.ButtonStyle.url, url=message.jump_url))
-
-    await log_channel.send(embed=embed, view=url_view)
-
 @client.tree.command()
 async def hello(interaction: discord.Interaction):
     """打招呼！"""
